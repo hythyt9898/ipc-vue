603e696c9dd4a00a598d0c2bd1ca64f5a5bf84df
suiteki <suiteki@163.com>
2018-02-07 23:47:08 +0800

    add dependence

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'babel-polyfill'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.js'
 
 Vue.config.productionTip = false
 
